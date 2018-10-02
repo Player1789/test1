78945fa3d0d9ae5f766b5860dd68da9716c4a784
Player1789 <41373588+Player1789@users.noreply.github.com>
2018-10-03 08:07:12 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
-// 스시 마우스 이벤트
-let sushiPhotos = document.querySelectorAll(".section_2 .container .photo");
-sushiPhotos.forEach(photo=>photo.addEventListener('mouseover',showInfo));
+// 음식 마우스 이벤트
+let sushiPhotos = document.querySelectorAll(".section_3 .container .contents");
+sushiPhotos.forEach(content=>content.addEventListener('mouseover',showInfo));
 sushiPhotos.forEach(photo => photo.addEventListener('mouseleave', hideInfo));
-function showInfo () {this.querySelector('.info').style.display="block"} ;
+function showInfo (){this.querySelector('.info').style.display="block"} ;
 function hideInfo() { this.querySelector('.info').style.display = "none" };
 
 // 핫밀 마우스 이벤트
@@ -22,34 +21,13 @@ function menuVisibleHandler() {
     if(menu.style.display=="flex"){
         menu.style.display="none"
     } else {
-        menu.style.display = "flex"
+        menu.style.display = "flex";
     }
 };
 function menuDefault () {
-    if(window.innerWidth>1000){
+    if(window.innerWidth>1200){
         menu.style.display = "flex"
     } else {
         menu.style.display = "none"
     }
 };
-
-//앵커 메뉴
-const menus = document.querySelectorAll('.header ul li span');
-menus.forEach(menu=>menu.addEventListener('click',anchorHandler));
-function anchorHandler(){
-    switch(this.innerHTML){
-        case "SUSHI":
-            console.log('sushi');
-            window.scrollTo(0, 2000);
-            break;
-        case "MEAL":
-            console.log('meal')
-            break;
-        case "MENU":
-            console.log('menu')
-            break;
-        case "ABOUT":
-            console.log('about')
-            break;
-    }
-}
