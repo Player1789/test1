41ef99c2e75a1bf314c5255a0e1a58148e247b1b
Player1789 <41373588+Player1789@users.noreply.github.com>
2018-10-06 14:37:12 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -66,10 +66,9 @@ const hikariSgin = document.querySelector('#pattern_1');
 const soysauce = document.querySelector('#soysauce');
 const sauceBottle = document.querySelector('#soysauce_bottle');
 window.addEventListener('scroll', function (){
-    let wScroll = window.pageYOffset;
-    console.log(wScroll);
-    hikariSgin.style.transform = `translateY(${wScroll/2.7}px)`;
-    if(window.innerWidth>1200){sauceBottle.style.transform = `translateY(${wScroll /4.5}px)`}
-    else { sauceBottle.style.transform = `translateY(${wScroll / 5.5}px)`};
-    soysauce.style.transform = `translateY(${wScroll /1.6}px)`;
+    if (window.innerWidth > 1200){
+        let wScroll = window.pageYOffset;
+        hikariSgin.style.transform = `translateY(${wScroll/2.7}px)`;
+        sauceBottle.style.transform = `translateY(${wScroll /4.5}px)`;
+        soysauce.style.transform = `translateY(${wScroll /1.6}px)`;}
 })
